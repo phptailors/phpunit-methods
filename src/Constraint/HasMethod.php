2666f005e75fbb4ed2a1c16be90b81346d42617d
ptomulik <pawel@tomulik.pl>
2026-04-23 12:53:24 +0000

    make psalm happy on 4.0

--- src/Constraint/HasMethod.php
+++ src/Constraint/HasMethod.php
@@ -21,15 +21,7 @@ use Tailors\PHPUnit\Methods\MethodSpecSyntaxError;
  */
 final class HasMethod extends Constraint
 {
-    /**
-     * @var MethodSpecInterface
-     */
-    private $methodSpec;
-
-    public function __construct(MethodSpecInterface $methodSpec)
-    {
-        $this->methodSpec = $methodSpec;
-    }
+    public function __construct(private readonly MethodSpecInterface $methodSpec) {}
 
     /**
      * @throws InvalidArgumentException
@@ -72,7 +64,7 @@ final class HasMethod extends Constraint
 
         try {
             $method = new \ReflectionMethod($other, $this->methodSpec->getName());
-        } catch (\ReflectionException $exception) {
+        } catch (\ReflectionException) {
             return false;
         }
 
@@ -80,11 +72,9 @@ final class HasMethod extends Constraint
     }
 
     /**
-     * @param mixed $other
-     *
      * @psalm-assert-if-true object|class-string|trait-string|interface-string $other
      */
-    private function ensureCanReflectAsClass($other): bool
+    private function ensureCanReflectAsClass(mixed $other): bool
     {
         return is_object($other) || (is_string($other) && (
             interface_exists($other) || class_exists($other) || trait_exists($other)

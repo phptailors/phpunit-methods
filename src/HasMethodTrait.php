76c66b22e6cd2a342c3891ceb2e772f1486e7404
ptomulik <pawel@tomulik.pl>
2026-04-24 13:27:27 +0000

    Merge branch '4.0' into 5.0

--- src/HasMethodTrait.php
+++ src/HasMethodTrait.php
@@ -13,7 +13,6 @@ namespace Tailors\PHPUnit;
 use PHPUnit\Framework\Constraint\Constraint;
 use PHPUnit\Framework\Constraint\LogicalNot;
 use PHPUnit\Framework\ExpectationFailedException;
-use SebastianBergmann\RecursionContext\InvalidArgumentException as RecursionContextInvalidArgumentException;
 use Tailors\PHPUnit\Constraint\HasMethod;
 
 trait HasMethodTrait

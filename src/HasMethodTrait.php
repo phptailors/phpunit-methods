0656e460894262a5856a9a706bb985df7543fc63
ptomulik <pawel@tomulik.pl>
2026-05-12 06:55:21 +0000

    make psalm happy on 1.0

--- src/HasMethodTrait.php
+++ src/HasMethodTrait.php
@@ -21,9 +21,7 @@ trait HasMethodTrait
     /**
      * Evaluates a \PHPUnit\Framework\Constraint\Constraint matcher object.
      *
-     * @param mixed      $value
-     * @param Constraint $constraint
-     * @param string     $message
+     * @param mixed $value
      *
      * @throws ExpectationFailedException
      * @throws RecursionContextInvalidArgumentException
